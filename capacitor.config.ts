722b571283926ed26a00b76d0bceb315d9f9ab1d
Daniel Lins <daniel.lins@vohu.com.br>
2026-02-05 22:46:11 +0000

    ajuste botao e status bar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,15 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false
+    }
+  },
+
   server: {
     androidScheme: "http"
-  },  
+  },
   appId: 'br.com.vohu.beautyplatform',
   appName: 'Beauty Platform',
   webDir: 'www'
